7128ee7d8f8fa45b745a8afe4dbaa821a1b8ed04
heondong9265 <heondong0723@gmail.com>
2023-12-05 18:05:39 +0900

    feat: CORS 허용

--- src/main/java/travel/project/springboot/travel/Attraction/controller/AttractionController.java
+++ src/main/java/travel/project/springboot/travel/Attraction/controller/AttractionController.java
@@ -2,10 +2,7 @@ package travel.project.springboot.travel.Attraction.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import travel.project.springboot.travel.Attraction.domain.entity.Attraction;
 import travel.project.springboot.travel.Attraction.dto.AttractionResponse;
 import travel.project.springboot.travel.Attraction.service.AttractionService;
