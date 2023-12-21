0c68a05b97214314961f76636d7f6f62617526c7
heondong9265 <heondong0723@gmail.com>
2023-12-21 23:50:44 +0900

    build: swagger setting

--- src/main/java/travel/project/springboot/global/config/SwaggerConfig.java
+++ src/main/java/travel/project/springboot/global/config/SwaggerConfig.java
@@ -13,19 +13,6 @@ import java.util.Arrays;
 @Configuration
 public class SwaggerConfig {
 
-    @Bean
-    public OpenAPI openAPI() {
-        SecurityScheme securityScheme = new SecurityScheme()
-                .type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")
-                .in(SecurityScheme.In.HEADER).name("Authorization");
-        SecurityRequirement securityRequirement = new SecurityRequirement().addList("bearerAuth");
-
-        return new OpenAPI()
-                .components(new Components().addSecuritySchemes("bearerAuth", securityScheme))
-                .security(Arrays.asList(securityRequirement))
-                .info(apiInfo());
-    }
-
     private Info apiInfo() {
         return new Info()
                 .title("TravelProject API 테스트")
